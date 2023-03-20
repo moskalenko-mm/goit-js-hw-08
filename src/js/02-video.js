a11eaa0c2c7d7178f17ec6e18e068c9dcdc86392
Nicholas <nicholasmoskalenko@gmail.com>
2023-03-20 12:10:33 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
-console.log('qq');
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const localStorageKey = 'videoplayer-current-time';
+let currentTime = localStorage.getItem(localStorageKey);
+
+player.on('timeupdate', throttle(playTime, 1000));
+
+function playTime(event) {
+  localStorage.setItem(localStorageKey, event.seconds);
+}
+
+player.setCurrentTime(currentTime || 0);
